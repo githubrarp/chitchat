740dcb659611d3f23c9fa7e3b2bbd4cfb46a6bb6
ricarod <info@tecfreelance.com>
2021-10-27 23:25:08 -0500

    partially implementing templates

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
-import(
+import (
+	"html/template"
 	"net/http"
-	"text/template"
 )
 
 func main() {
@@ -13,23 +13,43 @@ func main() {
 	mux.HandleFunc("/", index)
 
 	server := &http.Server{
-		Addr: "0.0.0.0:8080",
+		Addr:    "0.0.0.0:8080",
 		Handler: mux,
 	}
 
 	server.ListenAndServe()
 }
 
+func index(w http.ResponseWriter, r *http.Request) {
 
-func index(w http.ResponseWriter, r *http.Request){
-	files := []string{
-		"templates/layout.html",
-		"templates/navbar.html",
-		"templates/index.html",
-	}
-	templates := template.Must(template.ParseFiles(files...))
 	threads, err := data.Threads()
-	if err == nil{
+	if err == nil {
+		_, err := session(w, r)
+		public_tmp_files := []string{
+			"templates/layout.html",
+			"templates/public.navbar.html",
+			"templates/index.html",
+		}
+		private_tmp_files := []string{
+			"templates/layout.html",
+			"templates/private.navbar.html",
+			"templates/index.html",
+		}
+		var templates *template.Template
+		if err != nil {
+			templates = template.Must(template.ParseFiles(public_tmp_files...))
+		}
 		templates.ExecuteTemplate(w, "layout", threads)
 	}
+	/*
+		files := []string{
+			"templates/layout.html",
+			"templates/navbar.html",
+			"templates/index.html",
+		}
+		templates := template.Must(template.ParseFiles(files...))
+		threads, err := data.Threads()
+		if err == nil{
+			templates.ExecuteTemplate(w, "layout", threads)
+		}*/
 }
